42956f50a54c484739edd2a0ca371426bca826f8
Er. Sajid Alam <msasajid01.pro@gmail.com>
2025-07-04 22:56:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ document.getElementById('downloadZip').addEventListener('click', () => {
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>${elements.name.value}'s Portfolio</title>
+  <title>${elements.name.value.split(' ')[0]}'s Portfolio</title>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
   <link rel="stylesheet" href="style.css" />
